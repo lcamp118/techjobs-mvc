49c1c849b7bbcdc0fcaa0a72679ea1e8972a87ac
Lynelle Camp <lcamp118@live.com>
2019-01-10 20:13:01 -0600

    Finished Tech Jobs Assignment

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,6 +4,7 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
@@ -24,4 +25,21 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
+    @RequestMapping(value="results")
+    public String results(Model model, @RequestParam String searchTerm, String searchType) {
+
+        ArrayList<HashMap<String, String>> jobs;
+
+        if (searchType.equals("all")) {
+        jobs = JobData.findByValue(searchTerm);
+        }
+        else {
+        jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+
+        model.addAttribute( "column", ListController.columnChoices);
+        model.addAttribute("jobs", jobs);
+                return "search";
+    }
 }
